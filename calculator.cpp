4ab9409dbb5262142465fddea4aec45ada94c6f3
Iceglitter632 <52899186+Iceglitter632@users.noreply.github.com>
2020-06-01 18:53:28 +0800

    added subtract function

--- calculator.cpp
+++ calculator.cpp
@@ -1,9 +1,22 @@
 #include<iostream>
 using namespace std;
 
+int add(int x,int y)
+{
+	return x+y;
+}
+
+int subtract(int x,int y)
+{
+	return x-y;
+}
+
 int main()
 {
 	int x,y;
 	cin>>x>>y;
+	cout<<add(x,y)<<endl;
+	cout<<subtract(x,y)<<endl;
 	return 0;	
-} 
+}
+
